1fd85508203cfb5f1418bade8495da1eece5484c
Alfakynz <alfakynz.nyxaeris@gmail.com>
2026-03-03 22:54:01 +0100

    Prevent reloadResources() twice
    - Prevent reloadResources() twice
    - Ensure resourcepacks dire
    - Fix some warning

--- src/main/java/online/carteris/hot_reload_resource_packs/HotReloadResourcePacks.java
+++ src/main/java/online/carteris/hot_reload_resource_packs/HotReloadResourcePacks.java
@@ -1,12 +1,12 @@
 package online.carteris.hot_reload_resource_packs;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.nio.file.Path;
 
 public class HotReloadResourcePacks implements ModInitializer {
--- src/main/java/online/carteris/hot_reload_resource_packs/HotReloader.java
+++ src/main/java/online/carteris/hot_reload_resource_packs/HotReloader.java
@@ -12,10 +12,16 @@ public class HotReloader extends Thread {
     static Logger logger;
     static MinecraftClient minecraft_client;
     static Path resource_packs_path;
+    static long recent_ping;
 
     public void run() {
         logger.info("Watching for changes in {}", resource_packs_path);
         var default_fs = FileSystems.getDefault();
+
+        // Track the latest reload time to avoid double events on (open,
+        // modify), for systems that trigger these as unique separate
+        // ENTRY_MODIFY
+        recent_ping = System.currentTimeMillis();
 
         try (var watch_service = default_fs.newWatchService()) {
             // watch the resourcepacks folder for changes
@@ -24,14 +30,14 @@ public class HotReloader extends Thread {
             // recursively visit the preexisting directories and watch them for changes too
             Files.walkFileTree(resource_packs_path, new SimpleFileVisitor<>() {
                @Override
-               public @NotNull FileVisitResult preVisitDirectory(Path dir, @NotNull BasicFileAttributes attrs) {
+               public @NotNull FileVisitResult preVisitDirectory(@NotNull Path dir, @NotNull BasicFileAttributes attrs) {
                    registerPath(watch_service, dir);
                    return FileVisitResult.CONTINUE;
                }
             });
 
-            // watch for file events forever (until the game closes)
-            while(true) {
+            // watch for file events until the thread is interrupted (until the game closes)
+            while (!Thread.currentThread().isInterrupted()) {
                 watchFileEvents(watch_service);
             }
         } catch (UnsupportedOperationException e) {
@@ -47,15 +53,27 @@ public class HotReloader extends Thread {
         try {
             // a "key" represents a registered directory
             var watch_key = watch_service.take();
+            // whether or not we will reload packs with this file event
+            var reload = false;
+
             // handle file events that occur in one of our registered directories
             for (WatchEvent<?> event : watch_key.pollEvents()) {
                 handleFileEvent(watch_service, watch_key, event);
+                reload = true;
             }
 
             // let the watch service know that we're done handling events for this directory
             watch_key.reset();
 
-            minecraft_client.reloadResources();
+            // Test if we just got here, is recent_ping within a seconds of the previous.
+            if ((System.currentTimeMillis() - recent_ping) < 1500) {
+                reload = false;
+            }
+            recent_ping = System.currentTimeMillis();
+
+            if (reload) {
+                minecraft_client.reloadResources();
+            }
 
         } catch (InterruptedException e) {
             logger.error("Got interrupted while trying to wait for file changes");
@@ -67,7 +85,9 @@ public class HotReloader extends Thread {
         // this looks weird, but we're essentially joining the watched path (.minecraft/resourcepacks/xyz) with the ...
         // ... name of the file that had an event occur. Trying to get the absolute path of the file on its own ...
         // ... using event.context().toAbsolutePath() will give us the wrong path for some reason.
-        var path = ((Path) watch_key.watchable()).resolve(((WatchEvent<Path>) event).context()).toAbsolutePath();
+        Path context = (Path) event.context();
+        Path baseDir = (Path) watch_key.watchable();
+        Path path = baseDir.resolve(context).toAbsolutePath();
 
         // register any new directories to be watched
         if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE && path.toFile().isDirectory()) {
@@ -77,6 +97,12 @@ public class HotReloader extends Thread {
 
     void registerPath(WatchService watch_service, Path path) {
         try {
+            // Ensure resourcepacks path exists by creating it if it doesn't. 
+            if (!Files.exists(path)) {
+                Files.createDirectories(path);
+                logger.info("Created missing resourcepacks directory: {}", path);
+            }
+
             path.register(
                     watch_service,
                     StandardWatchEventKinds.ENTRY_CREATE,
